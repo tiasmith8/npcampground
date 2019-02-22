4478e5cd3f357037b7c0e115c57bb98d9b367b38
Noah Ginsburg <njginsburg@gmail.com>
2019-02-22 09:38:36 -0500

    creating campgroundsqldao tests

--- Capstone.Tests/CampgroundSqlDAOTests.cs
+++ Capstone.Tests/CampgroundSqlDAOTests.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Capstone.DAL;
+using Capstone.Models;
 
 namespace Capstone.Tests
 {
-    class CampgroundSqlDAOTests
+    [TestClass]
+    public class CampgroundSqlDAOTests : CapstoneDAOTests
     {
+
+        [TestMethod]
+        public void GetAllCampground_ReturnsNumberOfCampgrounds()
+        {
+            CampgroundSqlDAO dao = new CampgroundSqlDAO(ConnectionString);
+            IList<Campground> campgrounds = dao.GetAllCampgrounds(parkId);
+            Assert.AreEqual(1, campgrounds.Count);
+        }
     }
 }
