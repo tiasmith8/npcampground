3911916c69a0e920156e39578a6d40bad3599423
Tia Smith <tia.smith@gmail.com>
2019-02-22 09:56:13 -0500

    Updated test script

--- Capstone.Tests/ParkSqlDAOTests.cs
+++ Capstone.Tests/ParkSqlDAOTests.cs
@@ -1,10 +1,34 @@
-﻿using System;
+﻿using Capstone.DAL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Capstone.Models;
 
 namespace Capstone.Tests
 {
-    class ParkSqlDAOTests
+    [TestClass]
+    public class ParkSqlDAOTests : CapstoneDAOTests
     {
+
+        [TestMethod]
+        public void GetAllParks()
+        {
+            //Arrange
+            ParkSqlDAO parkDao = new ParkSqlDAO(ConnectionString);
+            IList<Park> parks = new List<Park>();
+
+            //Act - Get a list of parks that should be 1 based on test data
+            parks = parkDao.GetAllParks();
+
+            //Assert
+            Assert.AreEqual(1, parks.Count);
+
+        }
+
+
+
+
+
     }
 }
--- Capstone.Tests/test-script.sql
+++ Capstone.Tests/test-script.sql
@@ -1,8 +1,8 @@
 ﻿-- DELETE DATABASE DATA
-DELETE FROM park
-DELETE FROM campground
-DELETE FROM site
-DELETE FROM reservation
+DELETE FROM reservation;
+DELETE FROM [site];
+DELETE FROM campground;
+DELETE FROM park;
 
 -- INSERT sample park
 INSERT INTO park VALUES ('Tia Noah Land','The Moon','1701-01-01',10000,2,'The moon oh moon what can I say oh moon? Tis dark and spacy and glorious to behold. Twas a sky twas a night to whit to woo oh what a moon!')
@@ -23,4 +23,3 @@ DECLARE @reservationId int = (SELECT @@IDENTITY);
 -- Return reservation ID
 SELECT @reservationId as reservationId;
 SELECT @parkId as parkId;
-
